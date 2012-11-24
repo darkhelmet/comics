76124f9b52e4a125d479204195c32e1ce335acf8
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2012-11-24 00:58:44 -0700

    Better proxy for comics

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'sinatra', '~> 1.3.3', :require => nil
+gem 'activesupport', '~> 3.2.9', :require => 'active_support/all'
+gem 'nokogiri', '~> 1.5.5'
+gem 'builder', '~> 3.1.4'
+gem 'dalli', '~> 2.5.0', :require => ['dalli', 'active_support/cache/dalli_store']
+gem 'rest-client', '~> 1.6.7'
+gem 'celluloid', '~> 0.12.3'
+gem 'puma', '~> 1.6.3'
---
+++ comics.rb
@@ -0,0 +1,71 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra'
+require 'downloader'
+
+configure do
+  disable :lock
+  set :cache, ActiveSupport::Cache::DalliStore.new(expires_in: 1.day)
+end
+
+helpers do
+  def with_rss(url)
+    settings.cache.fetch(url) do
+      resp = RestClient.get(url)
+      doc = Nokogiri::XML(resp)
+      yield(doc)
+      doc.to_s
+    end
+  end
+
+  def remove_unless_title_match(doc, re)
+    doc.search('item title').each do |title|
+      title.parent.remove unless title.text.match(re)
+    end
+  end
+
+  def with_each_link_and_page(doc)
+    links = doc.search('item link')
+    downloader_pool = Downloader.pool(size: links.count, args: [settings.cache])
+    map = links.reduce({}) do |map, link|
+      map.merge!(link => downloader_pool.future.get_and_parse(link.text))
+    end
+
+    links.each do |link|
+      yield(link, map[link].value)
+    end
+  end
+
+  def replace_description_with(doc, link, str)
+    cdata = Nokogiri::XML::CDATA.new(doc, str)
+    desc = link.parent.at('description')
+    desc.children.each(&:remove)
+    desc.add_child(cdata)
+  end
+end
+
+get '/' do
+  'Hello, World!'
+end
+
+get '/explosm' do
+  content_type :rss
+  with_rss('http://feeds.feedburner.com/Explosm') do |doc|
+    remove_unless_title_match(doc, /\d{2}\.\d{2}\.\d{4}/)
+    with_each_link_and_page(doc) do |link, page|
+      img = page.at('img:first[alt="Cyanide and Happiness, a daily webcomic"]')
+      replace_description_with(doc, link, img.to_s)
+    end
+  end
+end
+
+get '/cad' do
+  content_type :rss
+  with_rss('http://www.cad-comic.com/rss/rss.xml') do |doc|
+    remove_unless_title_match(doc, /^Ctrl/)
+    with_each_link_and_page(doc) do |link, page|
+      img = page.at('#content > img:first')
+      replace_description_with(doc, link, img.to_s)
+    end
+  end
+end
